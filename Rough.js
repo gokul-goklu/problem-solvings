b09cf2a4f9006ad93d0caf294c202c8fe2bae446
gokul-goklu <gokulshanmugam062@gmail.com>
2023-08-23 20:20:21 +0530

    First committ

--- Rough.js
+++ Rough.js
@@ -1,51 +1,19 @@
-const arr = [8, 8, 6, 5, 4, -3, 2, 1];
+const arr = "[{()}]";
 
-function check(arr, k) {
-  const positiveArr = [];
-  const negativeArr = [];
-  let klargest = 0;
+const paranthesis = (arr) => {
+  const mapping = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  let arr2 = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 0) {
-      positiveArr[arr[i]] = positiveArr[arr[i]] || 0;
-      positiveArr[arr[i]] = positiveArr[arr[i]] + 1;
-    }
-
-    if (arr[i] < 0) {
-      let curr = Math.abs(arr[i]);
-      negativeArr[curr] = negativeArr[curr] || 0;
-      negativeArr[curr] = negativeArr[curr] + 1;
-    }
-  }
-  let target = 0;
-  for (let i = positiveArr.length - 1; i >= 0; i--) {
-    if (target < k) {
-      if (positiveArr[i]) {
-        //  console.log(i);
-        target = target + 1;
-      }
-    }
-
-    if (target === k) {
-      klargest = i;
-      break;
-    }
+    if (arr[i] == "[" || arr[i] == "{" || arr[i] == "(") arr2.push(arr[i]);
+    else if (arr2[arr2.length - 1] !== mapping[arr[i]]) arr2.pop();
   }
 
-  if (target < k) {
-    for (let i = 0; i < negativeArr.length; i++) {
-      if (target < k) {
-        if (negativeArr[i]) {
-          // console.log(i);
-          target = target + 1;
-        }
-      }
-      if (target === k) {
-        klargest = `-${i}`;
-        break;
-      }
-    }
-  }
-  console.log(klargest);
-}
+  if (arr2.length == 0) return true;
+  if (arr2.length !== 0) return false;
+};
 
-check(arr, 1);
+console.log(paranthesis(arr));
